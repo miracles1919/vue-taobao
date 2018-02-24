2e4390924f437b3f7ff9c54033137fdbeae2c091
miracles <miracles@fanfandata.com>
2018-02-24 23:29:17 +0800

    增加less-loader，Header组件开发

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,21 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <h2>Ecosystem</h2>
+    <Header></Header>
   </div>
 </template>
 
 <script>
+import Header from './Layout/Header'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'hello world'
     }
+  },
+  components: {
+    Header
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="head_bg">
+    <div class="nav">
+      <ul class="nav_left">
+        <li>
+          <span>中国大陆</span>
+        </li>
+        <li v-on:mouseenter="hover" v-on:mouseleave="blur">
+          <span>遮不住的时光</span>
+        </li>
+        <li><span>消息</span></li>
+        <li><span>手机逛淘宝</span></li>
+      </ul>
+      <ul class="nav_right"></ul>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+  .head_bg {
+    width: 100%;
+    background-color: #f5f5f5;
+    border-bottom: 1px solid #eee;
+  }
+  .nav {
+    margin: 0 auto;
+    width: 1190px;
+    height: 35px;
+    font-size: 12px;
+  }
+  .nav_left {
+    display: flex;
+
+    li {
+      line-height: 35px;
+      padding: 0 6px;
+      cursor: pointer;
+
+      span:hover {
+        color: #f40;
+      }
+    }
+  }
+  .nav_hover {
+    background-color: #fff;
+  }
+</style>
+
+<script>
+export default {
+  name: 'Header',
+  methods: {
+    hover: function (e) {
+      e.target.className += ' nav_hover'
+    },
+
+    blur: function (e) {
+      e.target.className = e.target.className.replace(/nav_hover/g, '')
+    }
+  }
+}
+</script>
