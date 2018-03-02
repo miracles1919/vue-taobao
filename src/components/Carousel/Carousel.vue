0f647a8f57df29f84df0a2a0202f506b53c7d5cf
miracles <miracles@fanfandata.com>
2018-03-02 23:05:16 +0800

    完善轮播组件，增加左右点击箭头、底部

--- src/components/Carousel/Carousel.vue
+++ src/components/Carousel/Carousel.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-bind:style="{ width: width + 'px', height: height + 'px' }" class="promo">
-      <div v-bind:style="{ width: width * (imgList.length + 1) + 'px', transform: `translate3d(${-translate}px, 0 ,0)`, transitionDuration: translateTime + 's'}" class="promo_bd">
+      <div v-bind:style="{ width: width * (imgList.length + 2) + 'px', transform: `translate3d(${-translate}px, 0 ,0)`, transitionDuration: translateTime + 's'}" class="promo_bd">
         <div class="banner">
           <img v-bind:src="imgList[imgList.length - 1].url" />
         </div>
@@ -13,6 +13,13 @@
           <img v-bind:src="imgList[0].url"/>
         </div>
       </div>
+      <div class="promo_opt">
+        <span class="arrow" @click="distance('prev')"><i class="iconfont">&#xe602;</i></span>
+        <span class="arrow" @click="distance('next')"><i i class="iconfont">&#xe603;</i></span>
+      </div>
+      <ul class="promo_nav">
+        <li v-for="n in imgList.length" :key="n"><i v-bind:class="[index + 1 === n ? 'selected' : '', 'spot']"/>{{ index+1 }} {{ n }}</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -35,6 +42,67 @@
       float: left;
     }
   }
+  .promo_opt {
+    transition: all .2s;
+    .arrow {
+      display: block;
+      height: 30px;
+      width: 20px;
+      line-height: 30px;
+      color: #fff;
+      position: absolute;
+      top: 50%;
+      background-color: rgba(0,0,0,.3);
+      cursor: pointer;
+      background-clip: padding-box;
+      i {
+        display: flex;
+        justify-content: center;
+        height: 30px;
+        width: 20px;
+        line-height: 30px;
+        color: #fff;
+        font-size: 14px;
+      }
+    }
+    .arrow:first-child {
+      border-bottom-right-radius: 15px;
+      border-top-right-radius: 15px;
+    }
+    .arrow:last-child {
+      right: 0;
+      border-bottom-left-radius: 15px;
+      border-top-left-radius: 15px;
+    }
+  }
+  .promo_nav {
+    border-radius: 10px;
+    position: absolute;
+    bottom: 15px;
+    height: 13px;
+    text-align: center;
+    font-size: 0;
+    left: 50%;
+    margin-left: -39px;
+    background-color: rgba(255,255,255,.3);
+
+    li {
+      display: inline-block;
+      margin: 3px;
+    }
+
+    .spot {
+      display: block;
+      padding-top: 8px;
+      width: 8px;
+      height: 0;
+      border-radius: 50%;
+      background: #fff;
+    }
+    .selected {
+      background: #ff5000;
+    }
+  }
 </style>
 
 <script>
@@ -44,31 +112,48 @@ export default {
   data () {
     return {
       translate: parseInt(this.width),
-      translateTime: 0.3
+      translateTime: 0.3,
+      index: 0
     }
   },
   mounted: function () {
-    this.start()
+    this.init()
   },
   methods: {
-    start: function () {
-      let width = this.width
+    init: function () {
+      setInterval(this.distance, 2000)
+    },
+    distance: function (type = 'next') {
+      let width = parseInt(this.width)
       let length = this.imgList.length
-      setInterval(() => {
-        let translate = this.translate
-        console.log(translate)
-        if (translate < width * length) {
-          this.translate += 520
-          if (translate === width * (length - 1)) {
-            setTimeout(() => {
-              this.translateTime = 0
-              this.translate = 0
-            }, 300)
-          } else {
-            this.translateTime = 0.3
-          }
+      let translate = this.translate
+      if (type === 'next') {
+        // 向右滑动
+        this.translate += width
+        this.index += 1
+        if (translate === width * length) {
+          setTimeout(() => {
+            this.translateTime = 0
+            this.translate = width
+          }, 300)
+          this.index = 0
+        } else {
+          this.translateTime = 0.3
         }
-      }, 2000)
+      } else if (type === 'prev') {
+        // 向左滑动
+        this.translate -= width
+        this.index -= 1
+        if (translate === width) {
+          setTimeout(() => {
+            this.translateTime = 0
+            this.translate = width * length
+          }, 300)
+          this.index = length - 1
+        } else {
+          this.translateTime = 0.3
+        }
+      }
     }
   }
 }
