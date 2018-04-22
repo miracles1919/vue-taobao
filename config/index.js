06757aac3a38f3d4b0be2cf9d90aba48f914b6ad
miracles <miracles@fanfandata.com>
2018-04-22 15:22:21 +0800

    add proxy api

--- config/index.js
+++ config/index.js
@@ -14,9 +14,9 @@ module.exports = {
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
-        // pathRewrite: {
-        //   '^/api': ''
-        // }
+        pathRewrite: {
+          '^/api': ''
+        }
       },
     },
 
