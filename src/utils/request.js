620f1552cc140fbf7dda6760cd96b63b0f234c5e
miracles <miracles@fanfandata.com>
2018-04-22 15:21:38 +0800

    增加请求方法

---
+++ src/utils/request.js
@@ -0,0 +1,21 @@
+const request = ({url, method = 'get', data = {}}) => {
+  const options = {
+    method,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  if (method === 'post') {
+    options.body = JSON.stringify(data)
+  }
+  return fetch(url, options)
+    .then(res => res.json())
+    .then(result => {
+      if (!result.success) {
+        alert(result.error)
+      }
+      return Promise.resolve({ ...result })
+    })
+}
+
+export default request
